d3b7a691d9ff65f6d3bf28706224e86212d076ac
Chris <krzysztof@ssvlabs.io>
2025-11-27 12:11:27 +0100

    Dist removal

--- dist/react.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-const react = require("./index-O5NlRrkT.js");
-exports.ComposeProvider = react.ComposeProvider;
-exports.useComposeConfig = react.useComposeConfig;
-exports.useSmartAccount = react.useSmartAccount;
--- dist/react.mjs
+++
@@ -1,6 +0,0 @@
-import { C, u, o } from "./index-jG7Y7gpl.mjs";
-export {
-  C as ComposeProvider,
-  u as useComposeConfig,
-  o as useSmartAccount
-};
